6b061bf0bd4982727e3b2ebdd93112b98480c81a
Norm Dole <nrdole@gmail.com>
2023-11-11 19:09:18 +0000

    Updated variables.tf

--- resource_group/variables.tf
+++ resource_group/variables.tf
@@ -1,1 +1,20 @@
-#
+variable "environment" {
+  description = "The deployment environment (e.g., dev, stg, prd ...recommend fix width names)"
+  type        = string
+}
+
+variable "spoke" {
+  description = "Configuration for the Azure provider"
+  type = object({
+    subscription = string
+  })
+}
+
+variable "all_rgs" {
+  description = "List of all resource groups with their properties"
+  type = list(object({
+    name   = string
+    region = string
+    tags   = map(string)
+  }))
+}
