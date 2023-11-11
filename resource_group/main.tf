dc1201f6219a106f9c3b1d5161fe513c23b9c98a
Norm Dole <nrdole@gmail.com>
2023-11-11 19:16:50 +0000

    Updated main.tf

--- resource_group/main.tf
+++ resource_group/main.tf
@@ -3,14 +3,12 @@ resource "azurerm_resource_group" "resource_group" {
   for_each = { for rgs in local.all_rgs : rgs.name => rgs }
   name     = each.value.name
   location = each.value.region
-  provider = azurerm.spoke
 
   # Refactored tags logic for better readability
   tags = merge(
     local.common_tags,
     {
       "Environment"  = var.environment
-      "Subscription" = var.spoke.subscription
       "Subsidiary"   = local.subsidiary_tag[each.key]
       "Appcode"      = upper(substr(each.value.name, 7, 3))
       "Region"       = each.value.region
